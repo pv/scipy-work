BUG: sparse: restrict CSR/CSC multidimensional fancy indexing to the broadcasting case (which is the only thing that works correctly)

scipy/sparse/csc.py
@@ -155,9 +155,14 @@ class csc_matrix(_cs_matrix):
                     if len(row.shape) == 1:
                         return self.T[col,row]
                     elif len(row.shape) == 2:
-                        row = row.reshape(-1)
-                        col = col.reshape(-1,1)
-                        return self.T[col,row].T
+                        col = np.atleast_2d(col)
+                        if row.shape[1] == 1 and col.shape[0] == 1:
+                            # broadcasting
+                            row = row.reshape(-1)
+                            col = col.reshape(-1,1)
+                            return self.T[col,row].T
+                        else:
+                            raise NotImplementedError('unsupported indexing')
                     else:
                         raise NotImplementedError('unsupported indexing')
 

scipy/sparse/csr.py
@@ -269,7 +269,12 @@ class csr_matrix(_cs_matrix):
                         #    val.append(self._get_single_element(i,j))
                         return np.asmatrix(val)
 
-                    elif len(row.shape) == 2:
+                    elif len(row.shape) == 2 and row.shape[1] == 1 and \
+                            (len(col.shape) == 2 and col.shape[0] == 1
+                             or len(col.shape) == 1):
+                        # broadcasting
+                        check_bounds(row, self.shape[0])
+                        check_bounds(col, self.shape[1])
                         row = np.ravel(row)                   #[[[1],[2]],[1,2]]
                         P = extractor(row, self.shape[0])
                         return (P*self)[:,col]