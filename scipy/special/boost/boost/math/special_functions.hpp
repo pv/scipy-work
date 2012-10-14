9b51887c1719ac0fa2186544324c595bfeec8094
Pauli Virtanen <pav@iki.fi>
2012-10-14 14:12:41 +0200

    special/boost: include all files reported by bcp --- this makes boost weigh 23 MB, apparently due to support for various not-fully-compliant compilers

--- scipy/special/boost/boost/math/special_functions.hpp
+++ scipy/special/boost/boost/math/special_functions.hpp
@@ -1,61 +1,41 @@
-//  Copyright John Maddock 2006, 2007.
-//  Copyright Paul A. Bristow 2006, 2007.
-
-//  Use, modification and distribution are subject to the
-//  Boost Software License, Version 1.0. (See accompanying file
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-// This file includes *all* the special functions.
-// this may be useful if many are used
-// - to avoid including each function individually.
-
-#ifndef BOOST_MATH_SPECIAL_FUNCTIONS_HPP
-#define BOOST_MATH_SPECIAL_FUNCTIONS_HPP
-
-#include <boost/math/special_functions/acosh.hpp>
-#include <boost/math/special_functions/asinh.hpp>
-#include <boost/math/special_functions/atanh.hpp>
-#include <boost/math/special_functions/bessel.hpp>
-#include <boost/math/special_functions/beta.hpp>
-#include <boost/math/special_functions/binomial.hpp>
-#include <boost/math/special_functions/cbrt.hpp>
-#include <boost/math/special_functions/cos_pi.hpp>
-#include <boost/math/special_functions/digamma.hpp>
-#include <boost/math/special_functions/ellint_1.hpp>
-#include <boost/math/special_functions/ellint_2.hpp>
-#include <boost/math/special_functions/ellint_3.hpp>
-#include <boost/math/special_functions/ellint_rc.hpp>
-#include <boost/math/special_functions/ellint_rd.hpp>
-#include <boost/math/special_functions/ellint_rf.hpp>
-#include <boost/math/special_functions/ellint_rj.hpp>
-#include <boost/math/special_functions/erf.hpp>
-#include <boost/math/special_functions/expint.hpp>
-#include <boost/math/special_functions/expm1.hpp>
-#include <boost/math/special_functions/factorials.hpp>
-#include <boost/math/special_functions/fpclassify.hpp>
-#include <boost/math/special_functions/gamma.hpp>
-#include <boost/math/special_functions/hermite.hpp>
-#include <boost/math/special_functions/hypot.hpp>
-#include <boost/math/special_functions/laguerre.hpp>
-#include <boost/math/special_functions/lanczos.hpp>
-#include <boost/math/special_functions/legendre.hpp>
-#include <boost/math/special_functions/log1p.hpp>
-#include <boost/math/special_functions/math_fwd.hpp>
-#include <boost/math/special_functions/next.hpp>
-#include <boost/math/special_functions/powm1.hpp>
-#include <boost/math/special_functions/sign.hpp>
-#include <boost/math/special_functions/sin_pi.hpp>
-#include <boost/math/special_functions/sinc.hpp>
-#include <boost/math/special_functions/sinhc.hpp>
-#include <boost/math/special_functions/spherical_harmonic.hpp>
-#include <boost/math/special_functions/sqrt1pm1.hpp>
-#include <boost/math/special_functions/zeta.hpp>
-#include <boost/math/special_functions/modf.hpp>
-#include <boost/math/special_functions/round.hpp>
-#include <boost/math/special_functions/trunc.hpp>
-#include <boost/math/special_functions/pow.hpp>
-#include <boost/math/special_functions/next.hpp>
-#include <boost/math/special_functions/owens_t.hpp>
-#include <boost/math/special_functions/hankel.hpp>
-
-#endif // BOOST_MATH_SPECIAL_FUNCTIONS_HPP
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>Boost Licence Dependency Information</title>
+</head>
+<body>
+<H1>Boost Licence Dependency Information</H1>
+<H2>Contents</h2>
+<pre><a href="#input">Input Information</a>
+<a href="#summary">Licence Summary</a>
+<a href="#details">Licence Details</a>
+<a href="#files">Files with no recognised license</a>
+<a href="#authors">Files with no recognised copyright holder</a>
+Moving to the Boost Software License...
+  <a href="#bsl-converted">Files that can be automatically converted to the Boost Software License</a>
+  <a href="#to-bsl">Files that can be manually converted to the Boost Software License</a>
+  <a href="#not-to-bsl">Files that can <b>NOT</b> be moved to the Boost Software License</a>
+  <a href="#need-bsl-authors">Authors we need to move to the Boost Software License</a>
+<a href="#copyright">Copyright Holder Information</a>
+<a href="#depend">File Dependency Information</a>
+</pre><a name="input"></a><h2>Input Information</h2>
+<P>The following Boost modules were checked:<BR></p><p>The Boost path was: <code>/home/pauli/prj/scipy/scipy/scipy/special/boost</code></P><p>The Boost version is: 1.51.0</P>
+<a name="summary"></a><h2>Licence Summary</h2>
+<a name="details"></a><h2>Licence Details</h2>
+<h2><a name="files"></a>Files With No Recognisable Licence</h2>
+<P>The following 0 files had no recognisable license information:</P><BLOCKQUOTE><P>
+</p></BLOCKQUOTE><h2><a name="authors"></a>Files With No Recognisable Copyright Holder</h2>
+<P>The following 0 files had no recognisable copyright holder:</P>
+<BLOCKQUOTE><P></p></BLOCKQUOTE><h2><a name="bsl-converted"></a>Files that can be automatically converted to the Boost Software License</h2>
+<P>The following 0 files can be automatically converted to the Boost Software License, but require manual verification before they can be committed to CVS:</P>
+<h2><a name="to-bsl"></a>Files that could be converted to the Boost Software License</h2>
+<P>The following 0 files could be manually converted to the Boost Software License, but have not yet been:</P>
+<BLOCKQUOTE><P></p></BLOCKQUOTE><h2><a name="not-to-bsl"></a>Files that can NOT be converted to the Boost Software License</h2>
+<P>The following 0 files cannot be converted to the Boost Software License because we need the permission of more authors:</P>
+<BLOCKQUOTE><P></p></BLOCKQUOTE><h2><a name="need-bsl-authors"></a>Authors we need for the BSL</h2>
+<P>Permission of the following authors is needed before we can convert to the Boost Software License. The list of authors that have given their permission is contained in <code>more/blanket-permission.txt</code>.</P>
+<BLOCKQUOTE><P></p></BLOCKQUOTE><H2><a name="copyright"></a>Copyright Holder Information</H2><table border="1">
+</table>
+<H2><a name="depend"></a>File Dependency Information</H2><BLOCKQUOTE><pre>
+</pre></BLOCKQUOTE>
+</body></html>
