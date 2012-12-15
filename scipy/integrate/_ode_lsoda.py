5a1ec9cea20ab071c8f79c44c43e5bbcb1420815
Pauli Virtanen <pav@iki.fi>
2012-12-15 18:37:20 +0200

    XXX: commit

---
+++ scipy/integrate/_ode_lsoda.py
@@ -0,0 +1,138 @@
+import numpy as np
+
+from _odebase import OdeSolverBase, NonReEntrantOdeSolverBase, OdeStepResult
+import _lsoda
+
+class LsodaSolver(NonReEntrantOdeSolverBase):
+
+    messages = {
+        2: "Integration successful.",
+        -1: "Excess work done on this call (perhaps wrong Dfun type).",
+        -2: "Excess accuracy requested (tolerances too small).",
+        -3: "Illegal input detected (internal error).",
+        -4: "Repeated error test failures (internal error).",
+        -5: "Repeated convergence failures (perhaps bad Jacobian or tolerances).",
+        -6: "Error weight became zero during problem.",
+        -7: "Internal workspace insufficient to finish (internal error)."
+    }
+
+    def __init__(self,
+                 rfn,
+                 jacfn=None,
+                 rtol=1e-6, atol=1e-12,
+                 lband=None, uband=None,
+                 nsteps=500,
+                 max_step=0.0,  # corresponds to infinite
+                 min_step=0.0,
+                 first_step=0.0,  # determined by solver
+                 ixpr=0,
+                 max_hnil=0,
+                 max_order_ns=12,
+                 max_order_s=5,
+                 method=None,
+                 f_params=None,
+                 jac_params=None
+                 ):
+
+        self.rfn = rfn
+
+        self.jacfn = jacfn
+        self.rtol = rtol
+        self.atol = atol
+        self.mu = uband
+        self.ml = lband
+
+        self.max_order_ns = max_order_ns
+        self.max_order_s = max_order_s
+        self.nsteps = nsteps
+        self.max_step = max_step
+        self.min_step = min_step
+        self.first_step = first_step
+        self.ixpr = ixpr
+        self.max_hnil = max_hnil
+
+        self.f_params = None
+        self.jac_params = None
+
+    def _reset(self, n):
+        # Calculate parameters for Fortran subroutine dvode.
+        if self.jacfn is not None:
+            if self.mu is None and self.ml is None:
+                jt = 1
+            else:
+                if self.mu is None:
+                    self.mu = 0
+                if self.ml is None:
+                    self.ml = 0
+                jt = 4
+        else:
+            if self.mu is None and self.ml is None:
+                jt = 2
+            else:
+                if self.mu is None:
+                    self.mu = 0
+                if self.ml is None:
+                    self.ml = 0
+                jt = 5
+        lrn = 20 + (self.max_order_ns + 4) * n
+        if jt in [1, 2]:
+            lrs = 22 + (self.max_order_s + 4) * n + n * n
+        elif jt in [4, 5]:
+            lrs = 22 + (self.max_order_s + 5 + 2 * self.ml + self.mu) * n
+        else:
+            raise ValueError('Unexpected jt=%s' % jt)
+        lrw = max(lrn, lrs)
+        liw = 20 + n
+        rwork = np.zeros((lrw,), float)
+        rwork[4] = self.first_step
+        rwork[5] = self.max_step
+        rwork[6] = self.min_step
+        self.rwork = rwork
+        iwork = np.zeros((liw,), np.intc)
+        if self.ml is not None:
+            iwork[0] = self.ml
+        if self.mu is not None:
+            iwork[1] = self.mu
+        iwork[4] = self.ixpr
+        iwork[5] = self.nsteps
+        iwork[6] = self.max_hnil
+        iwork[7] = self.max_order_ns
+        iwork[8] = self.max_order_s
+        self.iwork = iwork
+        self.args = [self.rfn, None, None, None,
+                     self.rtol, self.atol, 1, 1,
+                     self.rwork, self.iwork,
+                     self.jacobian, jt,
+                     self.f_params, 0,
+                     self.jac_params]
+
+    def init_step(self, t0, y0):
+        self.acquire_new_handle()
+
+        y0 = np.asarray(y0)
+        self._reset(y0.size)
+        self.args[1] = y0.copy()
+        self.args[2] = float(t0)
+        self.args[3] = float(t0)
+
+    def _step(self, t, y_out, itask):
+        self.check_handle()
+
+        self.args[3] = t
+        self.args[6] = itask
+
+        t, istate = _lsoda.lsoda(*self.args)
+        y_out[...] = self.args[1]
+        if istate < 0:
+            return OdeStepResult(success=False, t=t)
+        else:
+            # upgrade istate
+            self.args[7] = 2
+        return OdeStepResult(success=True, t=t)
+
+    def step(self, t_max, y_out):
+        return self._step(t_max, y_out, itask=1)
+
+    def step_auto(self, t_max, y_out):
+        self.rwork[0] = t_max
+        return self._step(t_max, y_out, itask=5)
---
+++ scipy/integrate/_odebase.py
@@ -0,0 +1,245 @@
+from collections import namedtuple
+import numpy as np
+
+__all__ = ['OdeSolverBase', 'OdeConcurrencyError', 'NonReEntrantOdeSolverBase']
+
+OdeSolveResult = namedtuple("OdeSolveResult",
+                            ['success', 't', 'y', 't_err', 'y_err'])
+
+OdeStepResult = namedtuple("OdeStepResult",
+                           ['success', 't'])
+
+class OdeSolverBase(object):
+    """
+    The interface which ODE solvers must implement.
+    """
+
+    dtype = np.dtype(float)
+
+    def __init__(self, rfn, **options):
+        """ 
+        Initialize the ODE Solver and it's default values 
+
+        Parameters
+        ----------
+        rfn
+            Right-hand-side function, with signature::
+
+                def rfn(t, y, out):
+                    out[...] = something
+
+        options
+            Additional options for initialization.
+        """
+        raise NotImplementedError('all ODE solvers must implement this')
+
+    def solve(self, tspan, y0):
+        """
+        Runs the solver.
+        
+        Parameters
+        ----------
+        tspan : array-like
+            Times at which the computed value will be returned.
+            Must contain the start time.
+        y0 : array-like
+            Initial values.
+
+        Returns
+        -------
+        result : OdeSolveResult
+            A named tuple with key names:
+            
+            success
+                indicating return status of the solver
+            t
+                numpy array of times at which the computations were successful
+            y
+                numpy array of values corresponding to times t (values of y[i, :] ~ t[i])
+            t_err
+                float or None - if recoverable error occured (for example reached maximum
+                number of allowed iterations), this is the time at which it happened
+            y_err
+                array of values corresponding to time t_err, or None
+        """
+        # Default implementation: solvers should override this for better efficiency.
+
+        tspan = np.asarray(tspan)
+        y0 = np.asarray(y0)
+
+        t = np.zeros(tspan.shape, dtype=float)
+        y = np.zeros(tspan.shape + y0.shape, dtype=self.dtype)
+
+        if len(tspan) > 0:
+            self.init_step(tspan[0], y0)
+            y[0] = y0
+
+        for j in xrange(0, len(tspan)):
+            r = self.step(t[j], y[j])
+            if not r.success:
+                return OdeSolveResult(success=False, t=t[:j], y=y[:j], t_err=None, y_err=None)
+
+        return OdeSolveResult(success=True, t=t, y=y, t_err=None, y_err=None)
+
+    def solve_auto(self, t_max, y0, maxiter=100000):
+        """
+        Runs the solver, determining the time points automatically.
+
+        Parameters
+        ----------
+        t_max : float
+            Time point to solve until.
+        y0 : array-like
+            Initial values.
+        maxiter : int, optional
+            Maximum number of iterations to perform.
+
+        Returns
+        -------
+        result : OdeSolveResult
+            A named tuple with key names:
+            
+            success
+                indicating return status of the solver
+            t
+                numpy array of times at which the computations were successful
+            y
+                numpy array of values corresponding to times t (values of y[i, :] ~ t[i])
+            t_err
+                float or None - if recoverable error occured (for example reached maximum
+                number of allowed iterations), this is the time at which it happened
+            y_err
+                array of values corresponding to time t_err, or None
+        """
+        # Default implementation: solvers should override this for better efficiency.
+
+        t_max = float(t_max)
+        y0 = np.asarray(y0)
+
+        nt = 0
+        t = np.zeros((100,), dtype=float)
+        y = np.zeros((100,) + y0.shape, dtype=self.dtype)
+
+        while True:
+            for j in range(nt, min(maxiter, len(t))):
+                r = self.step_auto(t_max, y[j])
+                if not r.success:
+                    return OdeSolveResult(success=False, t=t[:j], y=y[:j], t_err=None, y_err=None)
+                elif r.t >= t_max:
+                    t[j] = r.t
+                    return OdeSolveResult(success=True, t=t[:j], y=y[:j], t_err=None, y_err=None)
+                t[j] = r.t
+
+            nt = j + 1
+
+            if nt == maxiter:
+                return OdeSolveResult(success=False, t=t[:j], y=y[:j], t_err=None, y_err=None)
+
+            t.resize(2*nt + 1)
+            y.resize(t.shape[0], y.shape[1])
+
+    def init_step(self, t0, y0):
+        """
+        Initializes the solver and allocates memory.
+
+        Parameters
+        ----------
+        t0 : array-like
+            Initial time.
+        y0 : array-like
+            Initial condition for y.
+        """
+        raise NotImplementedError('all ODE solvers must implement this')
+
+    def step(self, t, y_out):
+        """
+        Method for calling successive next step of the ODE solver to
+        allow more precise control over the solver. The 'init_step'
+        method has to be called before the 'step' method.
+
+        Parameters
+        ----------
+        t : float
+            If t > 0.0 then integration is performed until this time
+            and results at this time are returned in y_retn.
+
+            If t < 0.0 only one internal step is perfomed towards time abs(t)
+            and results after this one time step are returned.
+        y_out : array-like
+            Array in which the computed value will be stored.
+            Needs to be preallocated.
+
+        Returns
+        -------
+        result : OdeStepResult
+            A named tuple with keys
+
+            success
+                status of the computation (successful or error occured)
+            t
+                time, where the solver stopped (when no error occured, this
+                is equal to the requested time)
+
+        """
+        raise NotImplementedError('all ODE solvers must implement this')
+
+    def step_auto(self, t_max, y_out):
+        """
+        Take a step of length determined internally by the solver.
+
+        Method for calling successive next step of the ODE solver to
+        allow more precise control over the solver. The 'init_step'
+        method has to be called before the 'step_auto' method.
+
+        Parameters
+        ----------
+        t_max : float
+            
+        y_out : array-like
+            Array in which the computed value will be stored.
+            Needs to be preallocated.
+
+        Returns
+        -------
+        result : OdeStepResult
+            A named tuple with keys
+
+            success
+                status of the computation (successful or error occured)
+            t_out
+                time, where the solver stopped (can be < t_max even
+                when successful)
+
+        """
+        raise NotImplementedError('This method is not implemented by this ODE solver')
+
+
+class OdeConcurrencyError(RuntimeError):
+    """
+    Failure due to concurrent usage of an integrator that can be used
+    only for a single problem at a time.
+
+    """
+    def __init__(self, name):
+        msg = ("ODE solver `%s` can be used to solve only a single problem "
+               "at a time. If you want to integrate multiple problems, "
+               "consider using a different solver ") % name
+        RuntimeError.__init__(self, msg)
+
+
+class NonReEntrantOdeSolverBase(OdeSolverBase):
+    """
+    Some of the integrators have internal state (ancient Fortran...),
+    and so only one instance can use them at a time.  This base class
+    provides simple tools for dealing with this.
+
+    """
+    _active_reentrant_handle = 0
+
+    def _acquire_new_handle(self):
+        self.__class__._active_reentrant_handle = self.__class__._active_reentrant_handle + 1
+        self._reentrant_handle = self.__class__._active_global_handle
+
+    def _check_handle(self):
+        if self._reentrant_handle is not self.__class__._active_global_handle:
+            raise OdeConcurrencyError(self.__class__.__name__)
