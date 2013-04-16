3bc1dd8682dde48b568ff9a3b41e22512adee24b
Daniel Smith <smithd.daniel.br@gmail.com>
2013-04-16 15:23:56 -0400

    Turned on fancy indexing tests for CSR

--- scipy/sparse/tests/test_base.py
+++ scipy/sparse/tests/test_base.py
@@ -1615,8 +1615,9 @@ def sparse_test_class(getset=True, slicing=True, slicing_assign=True,
 # Matrix class based tests
 #------------------------------------------------------------------------------
 
-class TestCSR(sparse_test_class(slicing_assign=False, fancy_assign=False,
-                                fancy_multidim_indexing=False)):
+class TestCSR(sparse_test_class()):
+    #slicing_assign=False, fancy_assign=False,
+    #                            fancy_multidim_indexing=False)):
     spmatrix = csr_matrix
 
     def test_constructor1(self):
@@ -1744,6 +1745,7 @@ class TestCSR(sparse_test_class(slicing_assign=False, fancy_assign=False,
             SIJ = SIJ.todense()
         assert_equal(SIJ, D[I,J])
 
+    """
     @dec.knownfailureif(True, "CSR not implemented")
     def test_slicing_3(self):
         pass
@@ -1751,7 +1753,7 @@ class TestCSR(sparse_test_class(slicing_assign=False, fancy_assign=False,
     @dec.knownfailureif(True, "CSR not implemented")
     def test_fancy_indexing_boolean(self):
         pass
-
+    """
 
 class TestCSC(sparse_test_class(slicing_assign=False, fancy_assign=False,
                                 fancy_multidim_indexing=False)):
