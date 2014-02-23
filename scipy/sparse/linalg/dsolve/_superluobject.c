539bd41b908e16714471b74982005cb419839e2c
Pauli Virtanen <pav@iki.fi>
2014-02-23 02:27:18 +0200

    YYY

--- scipy/sparse/linalg/dsolve/_superluobject.c
+++ scipy/sparse/linalg/dsolve/_superluobject.c
@@ -211,6 +211,7 @@ PyGetSetDef SuperLU_getset[] = {
     {"perm_c", SuperLU_getter, (setter)NULL, (char*)NULL, (void*)"perm_c"},
     {"U", SuperLU_getter, (setter)NULL, (char*)NULL, (void*)"U"},
     {"L", SuperLU_getter, (setter)NULL, (char*)NULL, (void*)"L"},
+    {"cond", SuperLU_getter, (setter)NULL, (char*)NULL, (void*)"cond"},
     NULL
 };
 
