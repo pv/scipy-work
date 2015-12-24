ecdbcee9929fd933efe356322088de994718a879
Ted Pudlik <tpudlik@gmail.com>
2015-12-24 14:47:25 -0500

    Fixed PEP8 compliance.

--- scipy/special/tests/test_spherical_bessel.py
+++ scipy/special/tests/test_spherical_bessel.py
@@ -98,8 +98,8 @@ def test_spherical_jn_yn_cross_product_1():
     # http://dlmf.nist.gov/10.50.E3
     n = np.array([1, 5, 8])
     x = np.array([0.1, 1, 10])
-    left = (  spherical_jn(n + 1, x)*spherical_yn(n,     x)
-            - spherical_jn(n,     x)*spherical_yn(n + 1, x))
+    left = (spherical_jn(n + 1, x) * spherical_yn(n, x) -
+            spherical_jn(n, x) * spherical_yn(n + 1, x))
     right = 1/x**2
     assert_allclose(left, right)
 
@@ -107,8 +107,8 @@ def test_spherical_jn_yn_cross_product_2():
     # http://dlmf.nist.gov/10.50.E3
     n = np.array([1, 5, 8])
     x = np.array([0.1, 1, 10])
-    left = (  spherical_jn(n + 2, x)*spherical_yn(n,     x)
-            - spherical_jn(n,     x)*spherical_yn(n + 2, x))
+    left = (spherical_jn(n + 2, x) * spherical_yn(n, x) -
+            spherical_jn(n, x) * spherical_yn(n + 2, x))
     right = (2*n + 3)/x**3
     assert_allclose(left, right)
 
