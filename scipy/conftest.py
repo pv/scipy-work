5b7c82fca0d276ab2d90c6d9fb90f1fdf48ee27e
Pauli Virtanen <pav@iki.fi>
2017-08-24 23:25:59 +0200

    TST: add refcount checking if possible

--- scipy/conftest.py
+++ scipy/conftest.py
@@ -2,6 +2,8 @@
 from __future__ import division, absolute_import, print_function
 
 import os
+import gc
+import sys
 import pytest
 import warnings
 
@@ -33,3 +35,40 @@ def check_fpu_mode(request):
         warnings.warn("FPU mode changed from {0:#x} to {1:#x} during "
                       "the test".format(old_mode, new_mode),
                       category=FPUModeChangeWarning, stacklevel=0)
+
+
+if hasattr(sys, 'gettotalrefcount'):
+    # For Python debug builds, check for reference leaks
+    @pytest.hookimpl()
+    def pytest_pyfunc_call(pyfuncitem):
+        testfunction = pyfuncitem.obj
+        funcargs = pyfuncitem.funcargs
+        testargs = {}
+        for arg in pyfuncitem._fixtureinfo.argnames:
+            testargs[arg] = funcargs[arg]
+
+        for k in range(2):
+            old_count = None
+            new_count = None
+
+            gc.collect()
+            old_count = sys.gettotalrefcount()
+            testfunction(**testargs)
+            gc.collect()
+            new_count = sys.gettotalrefcount()
+
+            if old_count == new_count:
+                # No leak
+                break
+
+            # Re-run to see if refcount stabilises
+            pyfuncitem.teardown()
+            pyfuncitem.setup()
+            gc.collect()
+        else:
+            raise AssertionError(
+                "reference leak: sys.gettotalrefcount() changed "
+                "by {0} during test".format(new_count - old_count))
+
+        return True
+
