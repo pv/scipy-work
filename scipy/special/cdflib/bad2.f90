33702f43d2cd3c480122b92b4c3d95007d3c1ded
Pauli Virtanen <pav@iki.fi>
2017-09-14 23:49:35 +0200

    snap

--- scipy/special/cdflib/bad2.f90
+++ scipy/special/cdflib/bad2.f90
@@ -1,8 +1,10 @@
 program main
   implicit none
-  double precision :: adn, b, xx, yy, dnterm
+  double precision :: adn, b, xx, yy, dnterm, dnterm_ex
   double precision :: alngam
   external :: alngam
+
+  dnterm_ex = -232.27341231994683d0
 
   adn = 5.0000000000000001D+099
   b = 1
@@ -10,5 +12,10 @@ program main
   yy = 1.3333333333333333D-101
   dnterm = alngam(adn+b)-alngam(adn+1.0D0)-alngam(b)+&
        adn*log(xx)+b*log(yy)
-  write(*,*) dnterm
+
+  if (abs(dnterm - dnterm_ex).lt.1d-3) then
+     write(*,*) 'OK'
+  else
+     write(*,*) dnterm, '!=', dnterm_ex
+  end if
 end program main
