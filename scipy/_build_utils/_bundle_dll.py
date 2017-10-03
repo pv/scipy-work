1edb329c649bcc06ced4a4f1f3b4084f2d7e07d7
Pauli Virtanen <pav@iki.fi>
2017-10-03 22:12:31 +0200

    snap

--- scipy/_build_utils/_bundle_dll.py
+++ scipy/_build_utils/_bundle_dll.py
@@ -1,4 +1,19 @@
+from __future__ import division, print_function, absolute_import
+
+import os
+from numpy.distutils.commands.build_ext import build_ext as old_build_ext
 
 
-def _bundle_msvcrt():
-    
+class build_ext(old_build_ext):
+    def run(self):
+        old_build_ext.run()
+
+        if self.compiler.compiler_type == 'msvc':
+            # Bundle the CRT
+            runtime_lib_dir = os.path.join(
+                self.build_lib, self.distribution.get_name(), 'extra-dll')
+
+            if not os.path.isdir(runtime_lib_dir):
+                pass
+
+            import pdb; pdb.set_trace()
